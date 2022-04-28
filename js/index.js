8899a0c019c58aabfc5099542a9a7082bc3085a9
MohamedMamdouh-98 <miido96@gmail.com>
2022-04-28 15:33:55 +0200

    Portfolio created

---
+++ js/index.js
@@ -0,0 +1,23 @@
+let list = document.querySelectorAll('.portfolio li');
+let box = Array.from(document.querySelectorAll('.portfolio .box'));
+
+list.forEach((li) =>{
+    li.addEventListener('click', removeAct)
+    li.addEventListener('click', hideCard)
+})
+//remove class active
+function removeAct(){
+    list.forEach((li) =>{
+        li.classList.remove('active')
+        this.classList.add('active')
+    })
+}
+//hide in cards
+function hideCard(){
+    box.forEach((img) =>{
+        img.style.display = 'none';
+        document.querySelectorAll(this.dataset.filter).forEach((e) =>{
+            e.style.display = 'flex';
+        })
+    })
+}
